021888530b1615b58c658ff50337159944ee777d
Resilient226 <zakjbowles@gmail.com>
2025-04-22 21:45:22 -0400

    Update track.js

--- API/track.js
+++ API/track.js
@@ -1,14 +1,11 @@
 export default async function handler(req, res) {
-  const employee = req.query.utm_source || "unknown";  // same as utm_source
+  const employee = req.query.utm_source || "unknown";
   const timestamp = new Date().toISOString();
 
   try {
-    await fetch("https://script.google.com/macros/s/AKfycbzN4kO9YNPVxPWPmqHd9EXFX2q33RJR0p5LcLKS8LkFB7AibRqqDFRJl2MI6jULJkn4aw/exec", {
+    await fetch("https://script.google.com/macros/s/AKfycbyJJ97Oq4uIHaPoSjJt32bWSn_3691P0R3_PfwMlYMIB1kcdkvRDtlbyykvrT9yA62_Tw/exec", {
       method: "POST",
-      body: JSON.stringify({ 
-        employee,
-        timestamp 
-      }),
+      body: JSON.stringify({ employee, timestamp }),
       headers: { "Content-Type": "application/json" }
     });
   } catch (err) {
